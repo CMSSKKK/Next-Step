4c4267d2ca372ea34764aa14139ce2cc3a5a760f
Minseok-Choi <cms05041@gmail.com>
2022-08-22 21:00:36 +0900

    feat:  @webServlet 어노테이션 작동 실패, 수동으로 서브릿 추가

--- src/main/java/next/WebServerLauncher.java
+++ src/main/java/next/WebServerLauncher.java
@@ -1,22 +1,38 @@
 package next;
 
 import java.io.File;
+
+import next.web.TestServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServlet;
 
 public class WebServerLauncher {
     private static final Logger logger = LoggerFactory.getLogger(WebServerLauncher.class);
 
     public static void main(String[] args) throws Exception {
-        String webappDirLocation = "webapp/";
+        String webappDirLocation = "webapp";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
 
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        Context context = tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
         logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        HttpServlet testServlet = new TestServlet();
+        String servletName = "hello";
+        String urlPattern = "/hello";
+        Tomcat.addServlet(context, servletName, testServlet);
+        context.addServletMappingDecoded(urlPattern, servletName);
 
         tomcat.start();
         tomcat.getServer().await();
     }
+
 }
